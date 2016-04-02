b7e49541e56930e672b77211d98a4049a515eaf4
Tim Birkett <tim@birkett-bros.com>
2016-04-02 11:38:20 +0100

    Tidy up unneeded files

--- docker.sh
+++
@@ -1,5 +0,0 @@
-docker-machine create --driver virtualbox --engine-env HTTP_PROXY=http://10.0.2.2:3128 --engine-env HTTPS_PROXY=http://10.0.2.2:3128 default
-
-eval $(docker-machine env default)
-
-docker-compose up
